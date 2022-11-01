389667de6449ac4aee69e226a17e1fcefe4b76ec
Quang Tran <quang.tran.769869@gmail.com>
2022-11-01 12:54:09 -0700

    added message model

---
+++ dizkord-backend/models/message.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+
+const messageSchema = new Schema({
+    auth: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    content: { type: String },
+    date: { type: Date },
+    type: { type: String }
+})
+
+module.exports = mongoose.model('Message', messageSchema)
