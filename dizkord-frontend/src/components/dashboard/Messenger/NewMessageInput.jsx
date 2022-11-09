6fb6240e0276d10ede02505b7d53d55fc02f2e1a
Quang Tran <quang.tran.769869@gmail.com>
2022-11-08 22:09:30 -0800

    updated channel messaging for handler

--- dizkord-frontend/src/components/dashboard/Messenger/NewMessageInput.jsx
+++ dizkord-frontend/src/components/dashboard/Messenger/NewMessageInput.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { styled } from "@mui/system";
 import { connect } from "react-redux";
 import { sendDirectMessage } from "../../../realtimeCommunication/socketConnection";
+import { sendChannelMessage } from "../../../realtimeCommunication/socketConnection";
 
 const MainContainer = styled('div')({
     height: '60px',
@@ -27,16 +28,26 @@ const NewMessageInput = ({ chosenChatDetails }) => {
     
     const [message, setMessage] = useState('')
     
+    const directOrGroupMessage = chosenChatDetails?.username ? 'DIRECT' : 'GROUP'
+    
     const handleMessageValueChange = (event) => {
         setMessage(event.target.value)
     }
 
     const handleSendMessage = () => {
         if (message.length > 0) {
-            sendDirectMessage({
-                receiverUserId: chosenChatDetails.id,
-                content: message
-            })
+            if (directOrGroupMessage === 'DIRECT') {
+                sendDirectMessage({
+                    receiverUserId: chosenChatDetails.id,
+                    content: message
+                })
+            } else if (directOrGroupMessage === 'GROUP') {
+                sendChannelMessage({
+                    channelId: chosenChatDetails.id,
+                    content: message
+                })
+            }
+
             setMessage('')
         }
     }
