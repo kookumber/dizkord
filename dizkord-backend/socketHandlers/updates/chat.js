19b5257e21cc4a6477fca1289bc0e1b8b63c980c
Quang Tran <quang.tran.769869@gmail.com>
2022-11-08 22:06:47 -0800

    added function to update channel chat history

--- dizkord-backend/socketHandlers/updates/chat.js
+++ dizkord-backend/socketHandlers/updates/chat.js
@@ -1,5 +1,7 @@
 const Conversation = require('../../models/conversation')
 const serverStore = require('../../serverStore')
+const Channel = require('../../models/channel')
+const Server = require('../../models/server')
 
 const updateChatHistory = async (conversationId, toSpecifiedSocketId = null) => {
     const conversation = await Conversation.findById(conversationId).populate({
@@ -28,7 +30,6 @@ const updateChatHistory = async (conversationId, toSpecifiedSocketId = null) => 
         conversation.participants.forEach(userId => {
             const activeConnections = serverStore.getActiveConnections(userId.toString())
 
-
             activeConnections.forEach(socketId => {
                 io.to(socketId).emit('direct-chat-history', {
                     messages: conversation.messages,
@@ -39,6 +40,53 @@ const updateChatHistory = async (conversationId, toSpecifiedSocketId = null) => 
     }
 }
 
+const updateChannelChatHistory = async (channelId, toSpecifiedSocketId = null) => {
+    const channel = await Channel.findById(channelId).populate({
+        path: 'messages',
+        model: 'Message',
+        populate: {
+            path: 'author',
+            model: 'User',
+            select: 'username _id'
+        }
+    })
+
+    if (channel) {
+        
+        const io = serverStore.
+        
+        getSocketServerInstance()
+        
+        const channelsServer = await Server.find({
+            channels: channelId
+        })
+        
+        if (channelsServer[0]) {
+            if (toSpecifiedSocketId) {
+                // Initial update of chat history
+                return io.to(toSpecifiedSocketId).emit('channel-chat-history', {
+                    messages: channel.messages,
+                    participants: channelsServer.participants
+                })
+            }
+
+            // For every participant of the given channel, we'll grab their id then look for active connections
+            // that those participants have, then emit to each connection the new messages
+            channelsServer[0].participants.forEach((participantId) => {
+                const activeConnections = serverStore.getActiveConnections(participantId.toString())
+
+                activeConnections.forEach((socketId) => {
+                    io.to(socketId).emit('channel-chat-history', {
+                        messages: channel.messages,
+                        participants: channelsServer.participants
+                    })
+                })
+            })
+        }
+    }
+}
+
 module.exports = {
-    updateChatHistory
+    updateChatHistory,
+    updateChannelChatHistory
 }
