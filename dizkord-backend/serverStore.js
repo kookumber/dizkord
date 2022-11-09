aa8c5eabe1b875024b8d59a3754d306e32ed45be
Quang Tran <quang.tran.769869@gmail.com>
2022-11-08 22:06:00 -0800

    removed console logs

--- dizkord-backend/serverStore.js
+++ dizkord-backend/serverStore.js
@@ -15,15 +15,15 @@ const getSocketServerInstance = () => {
 
 const addNewConnectedUser = ({ socketId, userId }) => {
     connectedUsers.set(socketId, { userId })
-    console.log('new connected users')
-    console.log(connectedUsers)
+    // console.log('new connected users')
+    // console.log(connectedUsers)
 };
 
 const removeConnectedUser = (socketId) => {
     if (connectedUsers.has(socketId)) {
         connectedUsers.delete(socketId)
         console.log('a user has disconnected')
-        console.log(connectedUsers)
+        // console.log(connectedUsers)
     }
 }
 
