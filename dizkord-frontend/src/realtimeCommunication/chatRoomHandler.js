b2f819c73836e567d1c5f88e80ef634cfad60c00
Quang Tran <quang.tran.769869@gmail.com>
2022-11-18 13:02:44 -0500

    built initial handlers to create/join chatrooms

--- dizkord-frontend/src/realtimeCommunication/chatRoomHandler.js
+++ dizkord-frontend/src/realtimeCommunication/chatRoomHandler.js
@@ -1,6 +1,42 @@
 import store from "../store/store"
-import { setOpenRoom } from "../store/actions/chatRoomActions"
+import { setActiveRooms, setOpenRoom, setRoomDetails } from "../store/actions/chatRoomActions"
+import * as socketConnection from './socketConnection'
 
 export const createNewChatRoom = () => {
     store.dispatch(setOpenRoom(true, true))
+    socketConnection.createNewRoom()
+}
+
+export const newRoomCreated = (data) => {
+    const { roomDetails } = data
+    store.dispatch(setRoomDetails(roomDetails))
+}
+
+export const updateActiveRooms = (data) => {
+    const { activeRooms } = data
+    
+    const friends = store.getState().friends.friends
+
+    const rooms = []
+
+    activeRooms.forEach((room) => {
+        friends.forEach((friend) => {
+            if (friend.id === room.roomCreator.userId) {
+                rooms.push({ ...room, creatorUsername: friend.username })
+            }
+        })
+    })
+
+    store.dispatch(setActiveRooms(rooms))
+
+}
+
+export const joinChatRoom = (roomId) => {
+    // Set the room details redux state for the user when they join the room
+    store.dispatch(setRoomDetails({ roomId }))
+
+    // run the setOpenRoom function and pass params of False for isUserCreator and True for isUserInRoom
+    store.dispatch(setOpenRoom(false, true))
+
+    socketConnection.joinChatRoom({ roomId })
 }
