13447aa4b162f6544fe8e1cb6b392b3b2e1d823f
Quang Tran <quang.tran.769869@gmail.com>
2022-11-22 13:33:58 -0500

    updated leaveRoom func to run chat room handler

--- dizkord-frontend/src/components/dashboard/ChatRoom/RoomButtons/CloseRoomButton.jsx
+++ dizkord-frontend/src/components/dashboard/ChatRoom/RoomButtons/CloseRoomButton.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import CloseIcon from '@mui/icons-material/Close'
 import IconButton from "@mui/material/IconButton";
+import * as chatRoomHandler from '../../../../realtimeCommunication/chatRoomHandler'
 
 const CloseRoomButton = () => {
 
     const handleLeaveRoom = () => {
-
+        chatRoomHandler.leaveChatRoom()
     }
 
     return (
