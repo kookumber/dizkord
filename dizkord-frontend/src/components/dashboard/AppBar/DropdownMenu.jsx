a7572510086e6b7012092583fcff0277f4533134
Quang Tran <quang.tran.769869@gmail.com>
2022-11-27 13:21:29 -0800

    added functionality to enable/disable only audio

--- dizkord-frontend/src/components/dashboard/AppBar/DropdownMenu.jsx
+++ dizkord-frontend/src/components/dashboard/AppBar/DropdownMenu.jsx
@@ -4,8 +4,10 @@ import MenuItem from '@mui/material/MenuItem';
 import { IconButton } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import { logout } from '../../../utils/auth';
+import { connect } from 'react-redux';
+import { getActions } from '../../../store/actions/chatRoomActions';
 
-export default function DropdownMenu() {
+const DropdownMenu = ({ audioOnly, setAudioOnly }) => {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
 
@@ -15,6 +17,10 @@ export default function DropdownMenu() {
     const handleMenuClose = () => {
         setAnchorEl(null);
     };
+
+    const handleAudioOnlyChange = () => {
+        setAudioOnly(!audioOnly)
+    }
 
     return (
         <div>
@@ -33,7 +39,24 @@ export default function DropdownMenu() {
                 {/* <MenuItem onClick={handleClose}>Profile</MenuItem>
                 <MenuItem onClick={handleClose}>My account</MenuItem> */}
                 <MenuItem onClick={logout}>Logout</MenuItem>
+                <MenuItem onClick={handleAudioOnlyChange}>
+                    { audioOnly ? "Audio Only Enabled" : "Audio Only Disabled" }
+                </MenuItem>
             </Menu>
         </div>
     );
 }
+
+const mapStoreStateToProps = ({ chatRoom }) => {
+    return {
+        ...chatRoom
+    }
+}
+
+const mapActionsToProps = (dispatch) => {
+    return {
+        ...getActions(dispatch)
+    }
+}
+
+export default connect(mapStoreStateToProps, mapActionsToProps)(DropdownMenu)
