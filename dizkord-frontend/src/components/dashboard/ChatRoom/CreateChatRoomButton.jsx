f8e2e1969ab40719a1ec6e8f6faa2378230d34c1
Quang Tran <quang.tran.769869@gmail.com>
2022-11-30 22:09:28 -0800

    modified to disable button if user in room

--- dizkord-frontend/src/components/dashboard/ChatRoom/CreateChatRoomButton.jsx
+++ dizkord-frontend/src/components/dashboard/ChatRoom/CreateChatRoomButton.jsx
@@ -3,15 +3,17 @@ import VideocamIcon from '@mui/icons-material/Videocam';
 import * as chatRoomHandler from '../../../realtimeCommunication/chatRoomHandler'
 
 
-const CreateChatRoomButton = () => {
+const CreateChatRoomButton = ({ isUserInRoom }) => {
 
     const createNewChatRoomHandler = () => {
         // Change store state to indicate user is in room
         chatRoomHandler.createNewChatRoom()
     }
 
+    console.log(isUserInRoom)
     return (
         <Button
+            disabled={isUserInRoom}
             onClick={createNewChatRoomHandler}
         >
             <VideocamIcon
