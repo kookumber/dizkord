6250889880fa0296e8330fd5c761903013cd2900
Quang Tran <quang.tran.769869@gmail.com>
2022-12-07 16:10:00 -0800

    testing server-side routing fix

--- dizkord-backend/server.js
+++ dizkord-backend/server.js
@@ -35,7 +35,7 @@ app.use(cors({ origin: true }));
 
 app.get('/*', (req, res) => {
     res.sendFile(
-        path.join(__dirname, 'dizkord-frontend/public/index.html'), 
+        path.join(__dirname, '../dizkord-frontend/public/index.html'), 
         (err) => { if (err) { res.status(500).send(err) }
     })
 })
